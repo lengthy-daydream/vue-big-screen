3e1037e825773e5305958813eb65d01004344c43
fantasy山一程 <18221421916@163.com>
2025-09-08 12:44:42 +0800

    更新依赖项，将 @jiaminghi/data-view 替换为 @kjgl77/datav-vue3，并添加 package-lock.json 文件以确保依赖版本一致性

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import dataV from '@jiaminghi/data-view';
+import dataV from '@kjgl77/datav-vue3';
 // 引入全局css
 import './assets/scss/style.scss';
 // 引入图表（所有图标见 icon 目录下的 demo_index.html）
